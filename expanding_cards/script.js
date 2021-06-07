df635f069c506a7b85eeffc440d5a661f097eb9e
mimirandi21 <mimirandi21@gmail.com>
2021-06-06 19:10:52 -0500

    update images

--- expanding_cards/script.js
+++ expanding_cards/script.js
@@ -1,6 +1,26 @@
 function changeAnimal(option) {
-	let chosenAnimal = document.querySelectorAll(`.${option}`);
-	chosenAnimal.forEach((animal) => {
-		animal.style.cssText += "display: inherit";
+	let cat = document.querySelector(".cat");
+	let dog = document.querySelector(".dog");
+	if (option === "cat") {
+		dog.style.cssText += "display: none";
+		cat.style.cssText += "display: flex";
+	} else {
+		cat.style.cssText += "display: none";
+		dog.style.cssText += "display: flex";
+	}
+}
+
+const panels = document.querySelectorAll(".panel");
+
+panels.forEach((panel) => {
+	panel.addEventListener("click", () => {
+		removeActiveClasses();
+		panel.classList.add("active");
+	});
+});
+
+function removeActiveClasses() {
+	panels.forEach((panel) => {
+		panel.classList.remove("active");
 	});
 }
