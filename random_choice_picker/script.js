1445e81053dd0c2bb39b02bed33ba8d642882911
mimirandi21 <mimirandi21@gmail.com>
2021-06-29 21:39:26 -0500

    finish random choice picker project

--- random_choice_picker/script.js
+++ random_choice_picker/script.js
@@ -34,11 +34,26 @@ function randomSelect() {
 
 	const interval = setInterval(() => {
 		const randomTag = pickRandomTag();
+
+		highlightTag(randomTag);
+		setTimeout(() => {
+			unhighlightTag(randomTag);
+		}, 100);
 	}, 100);
+
+	setTimeout(() => {
+		clearInterval(interval);
+
+		setTimeout(() => {
+			const randomTag = pickRandomTag();
+
+			highlightTag(randomTag);
+		}, 100);
+	}, times * 100);
 }
 
 function pickRandomTag() {
-	const tags = document.querySelectorAll(".tags");
+	const tags = document.querySelectorAll(".tag");
 	return tags[Math.floor(Math.random() * tags.length)];
 }
 
