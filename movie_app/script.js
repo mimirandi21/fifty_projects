e375c836d84df64c00915968b4ac0e7d1becee23
mimirandi21 <mimirandi21@gmail.com>
2021-07-06 13:20:24 -0500

    complete move app project

--- movie_app/script.js
+++ movie_app/script.js
@@ -4,10 +4,11 @@ const API_URL =
 const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
 
 const SEARCH_URL =
-	'https://api.themoviedb.org/3/search/movie?api_key=4731371be8b6c1e669d6eb50d0dd65a8&query"';
+	'https://api.themoviedb.org/3/search/movie?api_key=4731371be8b6c1e669d6eb50d0dd65a8&query="';
 
 const form = document.getElementById("form");
 const search = document.getElementById("search");
+const main = document.getElementById("main");
 
 getMovies(API_URL);
 
@@ -15,7 +16,41 @@ async function getMovies(url) {
 	const res = await fetch(url);
 	const data = await res.json();
 
-	console.log(data.results);
+	showMovies(data.results);
+}
+function showMovies(movies) {
+	main.innerHTML = "";
+	movies.forEach((movie) => {
+		const { title, poster_path, vote_average, overview } = movie;
+		const movieEl = document.createElement("div");
+		movieEl.classList.add("movie");
+		movieEl.innerHTML = `
+        
+				<img
+					src="${IMG_PATH + poster_path}"
+					alt="${title}"
+				/>
+				<div class="movie-info">
+					<h3>${title}</h3>
+					<span class="${getClassByRate(vote_average)}">${vote_average}</span>
+				</div>
+				<div class="overview">
+					<h3>Overview</h3>
+					${overview}
+				</div>
+			`;
+		main.appendChild(movieEl);
+	});
+}
+
+function getClassByRate(vote) {
+	if (vote >= 8) {
+		return "green";
+	} else if (vote >= 5) {
+		return "orange";
+	} else {
+		return "red";
+	}
 }
 
 form.addEventListener("submit", (e) => {
