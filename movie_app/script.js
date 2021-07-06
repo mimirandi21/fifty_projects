ab17babc20e5c99519d54ae6d39b4f6ff4f6995e
mimirandi21 <mimirandi21@gmail.com>
2021-07-06 13:03:34 -0500

    create api fetch

--- movie_app/script.js
+++ movie_app/script.js
@@ -1,1 +1,33 @@
+const API_URL =
+	"https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=4731371be8b6c1e669d6eb50d0dd65a8&page=1";
 
+const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
+
+const SEARCH_URL =
+	'https://api.themoviedb.org/3/search/movie?api_key=4731371be8b6c1e669d6eb50d0dd65a8&query"';
+
+const form = document.getElementById("form");
+const search = document.getElementById("search");
+
+getMovies(API_URL);
+
+async function getMovies(url) {
+	const res = await fetch(url);
+	const data = await res.json();
+
+	console.log(data.results);
+}
+
+form.addEventListener("submit", (e) => {
+	e.preventDefault();
+
+	const searchTerm = search.value;
+
+	if (searchTerm && searchTerm !== "") {
+		getMovies(SEARCH_URL + searchTerm);
+
+		search.value = "";
+	} else {
+		window.location.reload();
+	}
+});
