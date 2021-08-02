babe5eb1470767a3bfb24e9ab4c96369719ffe30
mimirandi21 <mimirandi21@gmail.com>
2021-08-02 11:04:09 -0500

    created functions for working js

--- toast_notifications/script.js
+++ toast_notifications/script.js
@@ -1,1 +1,37 @@
+let toasts = document.getElementById("toasts");
+let button = document.getElementById("button");
 
+// button.addEventListener("click", () => createNotification("wrong number"));
+
+// function createNotification(message) {
+// 	const notif = document.createElement("div");
+// 	notif.classList.add("toast");
+// 	notif.innerText = message;
+
+// 	toasts.appendChild(notif);
+// }
+
+const messages = [
+	"Message One",
+	"Message Two",
+	"Message Three",
+	"Message Four",
+];
+
+button.addEventListener("click", () => createNotification());
+
+function createNotification(message = null) {
+	const notif = document.createElement("div");
+	notif.classList.add("toast");
+	notif.innerText = message ? message : getRandomMessage();
+
+	toasts.appendChild(notif);
+
+	setTimeout(() => {
+		notif.remove();
+	}, 3000);
+}
+
+function getRandomMessage() {
+	return messages[Math.floor(Math.random() * messages.length)];
+}
