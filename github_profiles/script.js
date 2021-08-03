ea4b406601ca33217f1c280348f95630a75170d1
mimirandi21 <mimirandi21@gmail.com>
2021-08-03 15:06:50 -0500

    beginning js for axios requesting

--- github_profiles/script.js
+++ github_profiles/script.js
@@ -1,1 +1,21 @@
+const APIURL = "https://api.github.com/users/";
+const form = document.getElementById("form");
+const search = document.getElementById("search");
 
+form.addEventListener("submit", (e) => {
+	e.preventDefault();
+	const user = search.nodeValue;
+
+	if (user) {
+		getUser(user);
+		search.value = "";
+	}
+});
+
+async function getUser(username) {
+	try {
+		const { data } = await axios(APIURL + username);
+	} catch (err) {
+		console.log(err);
+	}
+}
